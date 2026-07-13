d9e5b66a3e8b23660e37007861fd486db83a0f91
Abhinav <1217abhinav@gmail.com>
2026-07-13 20:32:53 +0530

    Fix: make About section 3D decorative icons static again

--- src/App.tsx
+++ src/App.tsx
@@ -224,41 +224,33 @@ export default function App() {
       <section id="about" className="relative min-h-screen flex flex-col justify-center items-center px-5 sm:px-8 md:px-10 py-20 bg-[#0C0C0C] overflow-hidden z-20">
         {/* Decorative 3D Corner assets */}
         <FadeIn delay={0.1} x={-80} y={0} duration={0.9} className="absolute top-[4%] left-[1%] sm:left-[2%] md:left-[4%] z-10">
-          <motion.img
+          <img
             src="https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/moon_icon.11395d36.png"
             alt="Moon 3D icon"
-            animate={{ rotate: 360 }}
-            transition={{ repeat: Infinity, duration: 25, ease: "linear" }}
             className="w-[120px] sm:w-[160px] md:w-[210px] h-auto pointer-events-none select-none opacity-80"
           />
         </FadeIn>
 
         <FadeIn delay={0.25} x={-80} y={0} duration={0.9} className="absolute bottom-[8%] left-[3%] sm:left-[6%] md:left-[10%] z-10">
-          <motion.img
+          <img
             src="https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/p59_1.4659672e.png"
             alt="Swirl 3D object"
-            animate={{ rotate: -360 }}
-            transition={{ repeat: Infinity, duration: 30, ease: "linear" }}
             className="w-[100px] sm:w-[140px] md:w-[180px] h-auto pointer-events-none select-none opacity-80"
           />
         </FadeIn>
 
         <FadeIn delay={0.15} x={80} y={0} duration={0.9} className="absolute top-[4%] right-[1%] sm:right-[2%] md:right-[4%] z-10">
-          <motion.img
+          <img
             src="https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/lego_icon-1.703bb594.png"
             alt="Lego 3D icon"
-            animate={{ rotate: 360 }}
-            transition={{ repeat: Infinity, duration: 20, ease: "linear" }}
             className="w-[120px] sm:w-[160px] md:w-[210px] h-auto pointer-events-none select-none opacity-80"
           />
         </FadeIn>
 
         <FadeIn delay={0.3} x={80} y={0} duration={0.9} className="absolute bottom-[8%] right-[3%] sm:right-[6%] md:right-[10%] z-10">
-          <motion.img
+          <img
             src="https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/Group_134-1.2e04f3ce.png"
             alt="Group 3D object"
-            animate={{ rotate: -360 }}
-            transition={{ repeat: Infinity, duration: 35, ease: "linear" }}
             className="w-[130px] sm:w-[170px] md:w-[220px] h-auto pointer-events-none select-none opacity-80"
           />
         </FadeIn>
